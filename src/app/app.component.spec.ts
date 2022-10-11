c5f5696d883db9c2ef1d375ff45b57be1628bb0d
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-10-11 17:43:59 -0300

    build: add bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,11 +20,11 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'InstagramClone'`, () => {
+ /*  it(`should have as title 'InstagramClone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('InstagramClone');
-  });
+  });   */
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AcessModule } from './acess/acess.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    AcessModule
   ],
   providers: [],
   bootstrap: [AppComponent]
