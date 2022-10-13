d05211f6520f47a77ad6fa901633077b305f700f
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-10-13 19:11:08 -0300

    test: use mock as database

--- src/app/acess/login/login.component.ts
+++ src/app/acess/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserDataService } from './../../shared/services/userData.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [UserDataService]
 })
 export class LoginComponent {
+
+  constructor(private userData: UserDataService) {}
        private formLogin =  new FormGroup({
           user : new FormControl('',[Validators.required]),
           password : new FormControl('',[Validators.required, Validators.minLength(6)]),
@@ -17,4 +21,8 @@ export class LoginComponent {
         }
 
         ValidatorFormOnButton(): string { return this._FormLogin.valid ? '' : 'disabled' }
+
+        getUserConsult() {
+          console.log(this.userData.getUser(this._FormLogin.value.user,this._FormLogin.value.password))
+        }
 }
